7c8580a5c862b65ed4a55026d654c73acedf3c38
DESKTOP-NOER69M\ZBG <shariful11alam@gmail.com>
2024-12-30 18:53:08 +0600

    List implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,15 @@ package org.example;
 
 import java.util.*;
 
-///////////////////////////////////////Common methods in Collection///////////////////////////////////
+///////////////////////////////////////Common methods in Collections///////////////////////////////////
 //size(),isEmpty(),contains(),toArray(),add(),remove(),
 //addAll(),removeAll(),clear(),equals(),
 // stream() and parallelStream(),iterator()
 ///////////////////////////////////////
+///////////////////////////////////////Common methods in Iterator///////////////////////////////////
+//hasNext()-returns true if there are more elements in the collection
+//next()-return the next element,
+//remove()-remove the last element returned by the iterator
 
 public class Main {
     public static void main(String[] args) {
@@ -88,5 +92,99 @@ public class Main {
          * */
         //Thread safe version ConcurrentLinkedDeque
         //***************//
+
+
+        //*****************************************************************************************************************************************************//
+        ///////////////////////////////////////////////
+        //add(int index, E element)-insert element at the specific position
+        //addAll(int index, Collection c)-insert all elements at the specific position
+        //replaceAll(UniaryOperator op)-replace all element of the list, with result of applying operation
+        //sort(Comparator c)-sort based on the provided comparator
+        //get(int index)-return the element at a specific position
+        //set(int index, Element e)-replace the element at a specific index
+        //remove(int index)-remove the element from the index
+        //indexOf(Object o)-return index of the first occurrence of the object, -1 if no element found
+        //lastIndexOf(Object o)-return index of the last occurrence of the object, -1 if no element found
+        //ListIterator<E> listIterator()
+        //*****************************Methods of ListIterator************************//
+        //hasPrevious()-return true if there more elements in the list while traversing reverse order
+        //E previous()-return the previous element
+        //int nextIndex()-return the index of next element
+        //int previousIndex()-return the index of previous element
+        //set(E e)-set element
+        //add(E e)-add element
+        //ListIterator<E> listIterator(int index)
+        //List<E> subList(int fromIndex, int toIndex)//Any change in sublist will also change in main list
+        ///////////////////////////////////////////////
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(0,100);
+        list1.add(1,200);
+        list1.add(2,300);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(0,400);
+        list2.add(1,500);
+        list2.add(2,600);
+        list1.addAll(2,list2);
+        list1.forEach(val-> System.out.println(val));
+
+        list1.replaceAll(val->-1*val);
+        System.out.println("Print after replace");
+        list1.forEach(val-> System.out.println(val));
+
+        list1.sort((Integer val1, Integer val2)->val1-val2);
+        System.out.println("Sort list1");
+        list1.forEach(val-> System.out.println(val));
+
+        System.out.println("Value present in index 2: "+ list1.get(2));
+        list1.set(2,-4000);
+        System.out.println("After set method");
+        list1.forEach(val-> System.out.println(val));
+
+        list1.remove(2);
+        System.out.println("After removing");
+        list1.forEach(val-> System.out.println(val));
+
+        System.out.println("index of -200 Integer object is: " + list1.indexOf(-200));
+
+        ListIterator<Integer> listIterator1 = list1.listIterator(list1.size());
+        while (listIterator1.hasPrevious()) {
+            int prevVal = listIterator1.previous();
+            System.out.println("traversing backward: "+prevVal+" nextIndex: "+ listIterator1.nextIndex()+ " previousIndex: "+ listIterator1.previousIndex());
+            if(prevVal==-100) {
+                listIterator1.set(-50);
+            }
+        }
+        list1.forEach(val->System.out.println(val));
+
+        ListIterator<Integer> listIterator2 = list1.listIterator();
+        while (listIterator2.hasNext()) {
+            int val = listIterator2.next();
+            System.out.println("traversing forward: "+val+" nextIndex: "+ listIterator2.nextIndex()+ " previousIndex: "+ listIterator2.previousIndex());
+            if(val==-500) {
+                listIterator2.add(-400);
+            }
+        }
+        list1.forEach(val->System.out.println(val));
+
+        System.out.println("Sub List");
+        List<Integer> subList = list1.subList(1,4);
+        subList.forEach(val->System.out.println(val));
+        System.out.println("List1");
+        subList.add(-5);
+        list1.forEach(val->System.out.println(val));
+
+        //***************//
+        /* Complexity
+         * Insertion: O(1)-inserting add the end of the array
+         * O(n)-inserting element at a specific index
+         * O(n)-inserting add the end when threshold reached
+         * Deletion O(n)-have to shift all values
+         * Search O(1)
+         * */
+        //Thread safe version CopyOnWriteArrayList
+        //***************//
+
     }
 }
